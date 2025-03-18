fd3af44fbce5d1af28fbeac47adc92b9bead68be
Enzo-nz <enzo.camilo@escola.pr.gov.br>
2025-03-18 11:19:03 -0300

    18/03

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded,function' ()) {
+    const aumentarfontebotao = documenty. getElementyId('aumentar-font');
+    const diminuirfontebotao = documenty. getElementyId('aumentar-font');
+
+    let tamanhoAtualdaFonte= 1
+
+    aumentarfontebotao.addEventListener9('click'), function()
+}
