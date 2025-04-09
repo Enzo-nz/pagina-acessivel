fa2c753ea8ec8229a927c7be2c596e24d8c99ef3
Enzo-nz <enzo.camilo@escola.pr.gov.br>
2025-04-09 11:22:04 -0300

    09/04

---
+++ pagina-acessivel-main/site-acessivel-projeto-inicial/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const botaoAcessibilidade = document.getElementById(botaoAcessibilidade)
+    const opcoesAcessibilidade = document.getElementById(opcoesAcessibilidade)
+
+    botaoAcessibilidade.addEventListener'click', function(){
+        botaoAcessibilidade.classList.toggle(rotacao-botao);
+        opcoesAcessibilidade.classList.toggle(apresenta-lista)
+    }
+
+
+    const aumentarFonteBotao = document.getElementByld('aumentar-fonte')
+    const diminuirFonteBotao = document.getElementByld('diminuir-fonte')
+    let tamanhoAtualFonte= 1;
+
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0,1;
+        document.body.style.fontSize = ${tamanhoAtualFonte}rem
+    })
+    }
+    
+    )
+    diminuiFonteBotao.addEventListener('click', function){
+        tamanhoAtualFonte += 0,1;
+        document.body.style.fontSize = ${tamanhoAtualFonte}rem
+
+    }
