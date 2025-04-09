7c44be93ca8d6b989167b935af5de10d29c5c3f9
Enzo-nz <enzo.camilo@escola.pr.gov.br>
2025-04-09 12:42:28 -0300

    12/42

--- pagina-acessivel-main/script.js
+++ pagina-acessivel-main/script.js
@@ -1,12 +1,31 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const aumentaFonteBotao = document.getElementByld('aumentar-fonte')
-    const diminuiFonteBotao = document.getElementByld('diminuir-fonte')
-    let tamanhoAtualFonte= 1;
-    aumentaFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte += 0,1;
-        document.body.style.fontSize = '${tamanhoAtualFonte}rem'
-    })
-    }
+document.addEventListener('DOMContentLoaded',function(){
+
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDAcessibilidade= document.getElementById('opcoes-acessbilidade');
     
-    )
+    botaoDeAcessibilidade.addEventListener('click', function(){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+    })
+
+
+    const aumentarFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+    
+    let tamanhoAtualFonte= 1;
+    
+    aumentarFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+    diminuirFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+})
+
+alternaContraste.addEventListener('click', function () {
+    document.body.classList.toggle('alto-contraste')
+})
+})
     
